92b386f02ebcdbb5dd9ba746b6f7704f556eafb8
muriloorias <mu.a.o@outlook.com>
2026-02-09 20:54:04 -0300

    adding test post

--- routes/login.py
+++ routes/login.py
@@ -1,9 +1,24 @@
 #import
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 
 login_bp = Blueprint("login", __name__)
 
+emailtest = "email.teste@teste.com"
+passwordtest = "tesetest" 
+
 #requisição
 @login_bp.route('/login', methods=['GET'])
-def home():
-    return jsonify({"mensagem" : "olá"}), 200
+def verifyLoged(): 
+    return jsonify({"email" : f"{emailtest}",
+                    "password" : f"{passwordtest}"}), 200
+
+@login_bp.route('/login', methods=['POST'])
+def makeLogin():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    if email == emailtest and password == passwordtest:
+        return jsonify({"sucess": True})
+    else:
+        return jsonify({"sucess": False,
+                        "cause": "email and password is wrong"})
